8843b7c594548a13900fcadeec59f71835753769
mizou04 <1mizou1@gmail.com>
2023-08-13 20:11:45 +0100

    get basic commands from user interactively

--- url.rb
+++ url.rb
@@ -1,56 +1,95 @@
 #!/usr/bin/ruby
+require "io/console"
 require "open-uri"
 require "rexml"
 
 class InputError < IOError; end
-CURSOR="> "
+PROMPT="> "
+CURSOR="->"
+EOF="\003"
+R_IO=IO.new(0)
+W_IO=IO.new(1, "w")
 $urls = []
+COMMANDS=[]
+
+def usage
+  <<-USE
+  this is a simple RSS feed reader using terminal as UI
+  commands(after running): 
+                 h:     print this text
+                 r:     to restart the program
+    up/down arrows:     to move cursor
+
+  USE
+end
 
 def ask(qstn="include url:")
   $urls.clear
-  puts qstn
+  W_IO << qstn << "\n"
 end
 
 def getInput()
-  print CURSOR
-  urls = STDIN.readline
-  urls = urls.split " "
+  print PROMPT
+  urls = gets
+  urls = urls.split " " if urls.is_a? String
   urls.each do |u| 
-    u = u.chomp("\n")
     $urls.push(u) 
   end
   $urls.first
 end
 
-def idle()
-  input = gets
+def getCommand()
+  command = R_IO.getch
+  case command
+  when "h"
+    if COMMANDS.last != "help"
+      W_IO << usage
+      COMMANDS << "help"
+    end
+    getCommand()
+  when "r"
+    ask
+    COMMANDS << "read"
+  when EOF
+    exit
+  else
+    if COMMANDS.last != "help"
+      W_IO << usage
+      COMMANDS << "help"
+    end
+    getCommand()
+  end
 end
-
 
 def fetchXML
-  url = $urls[0]
-  file = URI.open(url) 
-  doc = REXML::Document.new(file)
-  root = doc.root
-  channel = root.elements["channel"]
-  channelTitle = channel.elements["title"].text
-  puts "\t\tfrom <<#{channelTitle}>>\t\t\n"
-  items = channel.get_elements("item")
-  items.each_with_index do |thing, i|
-    tTitle = thing.elements["title"].text
-    puts tTitle
-  end
-  #puts root.attributes
+  $urls.each do |url|
+		file = URI.open(url) 
+		doc = REXML::Document.new(file)
+		root = doc.root
+		channel = root.elements["channel"]
+		channelTitle = channel.elements["title"].text
+		W_IO << "\t\tfrom <<#{channelTitle}>>\t\t\n"
+		items = channel.get_elements("item")
+		items.each_with_index do |thing, i|
+			tTitle = thing.elements["title"].text
+      W_IO << "  -" << tTitle << "\n" * 2
+    end
+		end
+		#puts root.attributes
 end
 
+W_IO.write usage
+ask
 while(true) 
-  ask
   begin
-    getInput
+    getInput()
     fetchXML()
-    idle()
+    getCommand()
   rescue InputError, Errno::ENOENT, REXML::ParseException => e
     STDERR << e.message
     ask("try with a valid RSS url:")
+  rescue Interrupt
+    puts ""
+    exit(0) #exit with no errors
   end
 end
